a6471a6502ba303189aeba6761a6f38a3c8beb5b
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-04-28 20:03:49 +0100

    T005: feat(TxGenerator) Population — deterministic key/address derivation

--- test/unit-main.hs
+++ test/unit-main.hs
@@ -6,6 +6,7 @@ import Cardano.Node.Client.BalanceSpec qualified as BalanceSpec
 import Cardano.Node.Client.TxBuildGoldenSpec qualified as TxBuildGoldenSpec
 import Cardano.Node.Client.TxBuildSpec qualified as TxBuildSpec
 import Cardano.Node.Client.TxGenerator.PersistSpec qualified as TxGeneratorPersistSpec
+import Cardano.Node.Client.TxGenerator.PopulationSpec qualified as TxGeneratorPopulationSpec
 import Cardano.Node.Client.UTxOIndexer.IndexerSpec qualified as UTxOIndexerSpec
 import Cardano.Node.Client.UTxOIndexer.PersistenceSpec qualified as UTxOIndexerPersistenceSpec
 import Cardano.Node.Client.UTxOIndexer.ServerSpec qualified as UTxOIndexerServerSpec
@@ -23,3 +24,4 @@ main = hspec $ do
     UTxOIndexerServerSpec.spec
     UTxOIndexerPersistenceSpec.spec
     TxGeneratorPersistSpec.spec
+    TxGeneratorPopulationSpec.spec
